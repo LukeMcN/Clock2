55c960d70eae5f3c48910f3debf478aa1b6d7951
LukeMcN <43272812+LukeMcN@users.noreply.github.com>
2018-10-05 09:16:10 +0100

    Clock First  Save
    unfinished, 1st session

---
+++ NumberDisplay.java
@@ -0,0 +1,44 @@
+package clock;
+
+public class NumberDisplay 
+{
+
+	private int limit;
+	private int value;
+	
+	public NumberDisplay(int max){
+		
+		
+		limit = max;
+		value = 0;
+		
+				
+	}
+	
+	public int getValue(){
+		
+		return value;
+		
+	}
+	
+	public void setValue(){
+		
+		
+	}
+	
+	
+	public void DisplayValue(){
+		
+		
+		
+	}
+	
+	public void increment(){
+		
+		
+	}
+	
+	
+	
+	
+}
